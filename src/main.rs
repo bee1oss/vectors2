a3fbeaf3e36adc1a4b661f20e99992613fb5cea3
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-13 12:11:08 +0300

    vectors
    I learned how to store variables of different types in the same vector.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+#[derive(Debug)]
 enum Types {
-    
+    Int(i32),
+    Float(f64),
+    Bool(bool),
+    Text(String),
 }
 
 fn main() {
-    println!("Hello, world!");
+    let list = vec![
+        Types::Int(7),
+        Types::Float(7.7),
+        Types::Bool(true),
+        Types::Text("Hello".to_string()),
+    ];
+
+    match &list[3] {
+        Types::Int(i_num) => {
+            println!("Int is {}", i_num);
+        }
+        Types::Float(f_num) => {
+            println!("Float is {}", f_num);
+        }
+        Types::Bool(logic) => {
+            println!("Bool is {}", logic);
+        }
+        Types::Text(str) => {
+            println!("Text is {}", str);
+        }
+    }
 }
